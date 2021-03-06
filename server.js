e29c036ecf839444a0a516f8f151f8433b465140
Harun Or Rashid <horrashid007@gmail.com>
2021-03-06 23:58:42 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const app = express();
+
+// Set Template engine
+app.use(expressLayout);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+// Serve static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Server create
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
