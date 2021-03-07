5c42ff4628032105ae587049a4a3902740da4e4e
Harun Or Rashid <horrashid007@gmail.com>
2021-03-07 14:14:27 +0600

    database connection setup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const expressLayout = require('express-ejs-layouts');
 const webRouter = require('./routes/web');
+
 const app = express();
+dotenv.config();
 
 // Set Template engine
 app.use(expressLayout);
@@ -11,6 +15,23 @@ app.set('views', path.join(__dirname, '/resources/views'));
 
 // Serve static files
 app.use(express.static('public'));
+
+// Database connection
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+});
+
+const connection = mongoose.connection;
+connection
+  .once('open', () => {
+    console.log('DB connect successful!');
+  })
+  .catch((err) => {
+    console.log('DB connect fail!');
+  });
 
 // Web Routes
 app.use('/', webRouter);
