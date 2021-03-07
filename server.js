c4f3e10a991a60425f595410d40052a0d6724f96
Harun Or Rashid <horrashid007@gmail.com>
2021-03-07 12:31:34 +0600

    create login register page

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get('/cart', (req, res) => {
   res.render('customers/cart');
 });
 
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+
 // Server create
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
